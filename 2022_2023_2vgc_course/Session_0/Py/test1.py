270b0c029fad8acccae14dad601a1189d24d34ee
Manuel David Soto <78451509+mdsoto@users.noreply.github.com>
2026-03-09 17:49:47 +0100

    Delete 2022_2023_2vgc_course directory

--- 2022_2023_2vgc_course/Session_0/Py/test1.py
+++
@@ -1,5 +0,0 @@
-# x**3 program
-
-x = 5
-
-print('Result is', 3 * x)
--- 2022_2023_2vgc_course/Session_0/Py/test2.py
+++
@@ -1,5 +0,0 @@
-# Area of the circle program
-
-r = input('Radius is: ')
-
-print('Area of the circle is', 3.14151693 * float(r)**2)
